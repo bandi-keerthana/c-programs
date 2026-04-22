51e25ae6cc14b88e13d983050d6609d480794096
bandi-keerthana <keerthana939136@gmail.com>
2026-04-22 15:53:43 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,10 @@
+write a program to check the numberis even or odd
+#include <stdio.h>
+void main()
+{
+    int a;
+    printf("enter the a value");
+    scanf("%d",&a);
+    printf("a is the even number i.e is:%d",a%2==0);
+    
+}
