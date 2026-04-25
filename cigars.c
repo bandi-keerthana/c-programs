4c9d6fa43b266963fdf559312acc8b4721e41435
bandi-keerthana <keerthana939136@gmail.com>
2026-04-25 11:42:24 +0530

    Add files via upload

---
+++ cigars.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void main()
+{
+    int cigars;
+    printf("enter the number of cigars:\n");
+    scanf("%d",&cigars);
+    int weekend;
+    printf("enter 1 for weekend and 0 for weekdays");
+    scanf("%d",&weekend);
+    if(weekend==1)
+    {
+        if(cigars>=40)
+        {
+            printf("true");
+        }
+        else
+        {
+            if(cigars>=40&&cigars<=60)
+            {
+                printf("true");
+            }
+            else
+            {
+                printf("false");
+            }
+        }
+    }
+}
+    
+    
+    
+
