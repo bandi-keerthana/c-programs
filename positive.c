598eb9c1ec5f3e0aee8d7cb3191221727171fcd6
bandi-keerthana <keerthana939136@gmail.com>
2026-04-25 12:18:37 +0530

    Add files via upload

---
+++ positive.c
@@ -0,0 +1,10 @@
+write a program to check the number is positive or not
+#include <stdio.h>
+void main()
+{
+    int a;
+    printf("enter the a value");
+    scanf("%d",&a);
+    printf("a is the positive number i.e is:%d",a<0);
+    
+}
